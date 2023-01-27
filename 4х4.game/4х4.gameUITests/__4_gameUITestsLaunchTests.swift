1f6e6f131018f2b113f4baaff57816a1f08e69a2
Valeriaaa21 <lera19992104@mail.ru>
2023-01-28 01:27:05 +0600

    My first game
    4/4 memory cards game

---
+++ 4х4.game/4х4.gameUITests/__4_gameUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  __4_gameUITestsLaunchTests.swift
+//  4х4.gameUITests
+//
+//  Created by Валерия  on 17.01.2023.
+//
+
+import XCTest
+
+final class __4_gameUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
